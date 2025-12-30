8db67ae7da783ecab57dd608811a2b8b829d8c2e
John <wesatellite@bigmir.net>
2025-12-30 03:19:45 +0200

    8. EF relationships, add one-to-many migration

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,10 +1,22 @@
-﻿namespace API.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entities
 {
+    [Table("Photos")]
     public class Photo
     {
         public int Id { get; set; }
         public required string Url { get; set; }
         public bool IsMain { get; set; }
         public string? PublicId { get; set; }
+
+        // Navigation properties
+        // (Required one to many relationship)
+         public int AppUserId { get; set; }
+
+        //The = null!; construct is used to mark this as intentional for the C# compiler,
+        //since EF typically sets the AppUser instance 
+        //and it cannot be null for a fully loaded relationship. 
+        public AppUser AppUser { get; set; } = null!;
     }
 }
