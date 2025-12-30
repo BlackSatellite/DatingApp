3fc6588c65c89846a9410dc9d1a0e22ffac6ae84
John <wesatellite@bigmir.net>
2025-12-30 07:51:40 +0200

    8. Seed data

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,8 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,5 +29,29 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// It creates a scope for the application's services,
+// allowing for dependency injection and service resolution within that scope.
+
+// It uses pattern called the "service locator" to get hold of a service that we wish to use
+// outside of the normal dependency injection flow
+
+// And we want this scope to be disposed of
+// Because it is not coming via an HTTP request that we're doing something with our database
+// so we have to use different approach here
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
 
 app.Run();
